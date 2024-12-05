fe7c202beb1efdf05ef294a3588d26c218172953
vfaddey <vfaddey@yandex.ru>
2024-12-05 19:48:39 +0300

    initial

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.10.4-slim
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY generate_keys.py .
+CMD ["python3 generate_keys.py"]
+
+COPY . .
+EXPOSE 8000
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
---
+++ app/schemas/user.py
@@ -0,0 +1,27 @@
+import enum
+from typing import Optional
+
+from pydantic import BaseModel, UUID4, EmailStr
+
+
+class UserCreate(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone_number: Optional[str] = ''
+
+    class Config:
+        from_attributes = True
+
+
+class UserOut(UserCreate):
+    id: UUID4
+    email_verified: bool
+    phone_number_verified: bool
+
+
+class Rule(enum.Enum):
+    DELETE = 'delete'
+    UPDATE = 'update'
+    SUPER = 'super'
+
