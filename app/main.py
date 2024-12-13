7dbee27318691ffe85b434e370a265725b1d8181
vfaddey <vfaddey@yandex.ru>
2024-12-13 10:45:38 +0300

    fix origins

--- app/main.py
+++ app/main.py
@@ -4,12 +4,14 @@ from starlette.middleware.cors import CORSMiddleware
 from app.api.v1.auth import router as auth_router
 from app.api.v1.users import router as users_router
 from app.api.v1.oidc import router as oidc_router
+from app.core.config import settings
 
 app = FastAPI(
     docs_url="/docs",
     redoc_url="/redoc",
     openapi_url="/openapi.json",
-    root_path="/"
+    root_path="/",
+    title=settings.APP_NAME
 )
 app.include_router(auth_router)
 app.include_router(users_router)
@@ -18,7 +20,7 @@ app.include_router(oidc_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origin_regex="https?://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
